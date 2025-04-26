bba96f51b5fd78cc35282e28278d4fc928df8bcd
Uma Chandra <umachandra4821@gmail.com>
2025-04-26 06:45:16 +0530

    Add files via upload

---
+++ output.js
@@ -0,0 +1,4 @@
+console.log("This is output using console.log()");
+alert("This is using the alert()");
+document.writeln("This is output using document.write()");
+document.getElementById("output").innerHTML="This is output using innerhtml"
